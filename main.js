06161cbf02d6e775a61255c8526b8401e118b0fd
Tammy Dam <tdam@terpmail.umd.edu>
2020-04-02 22:20:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,3 @@
-/*const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
-
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');*/
-
-/* Looping through images */
-/*for (const a=0; a <= 5; a++) {
-  const newImage = document.createElement('img');
-  const nav= 'images/pic' + (a + 1).toString() + '.jpg';
-  newImage.setAttribute('src', nav);
-  thumbBar.appendChild(newImage);
-}*/
-
-    /* Darken/Lighten button */
-    
-
-/*btn.onclick = function() {
-  const btnClass = btn.getAttribute('class');
-    if (btn.getAttribute('class') === 'dark'){
-      btn.setAttribute('class','light');
-      btn.textContent = 'lighten';
-      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-    } 
-    
-    else {
-      btn.setAttribute('class','dark');
-      btn.textContent = 'darken';
-      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
-    }
-  }*/
-/* Source used:https://github.com/mdn/learning-area/blob/master/javascript/building-blocks/gallery/marking-guide.md*/
-
-
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
